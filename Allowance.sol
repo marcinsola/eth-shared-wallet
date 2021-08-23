1a377e81171e90d9f8a4b18521243e5aef9c45a6
Marcin Sola <marcinsolaonline@gmail.com>
2021-08-23 20:32:24 +0200

    Split smart contracts

---
+++ Allowance.sol
@@ -0,0 +1,32 @@
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Allowance is Ownable {
+    mapping(address => uint) public allowance;
+
+    event AllowanceChanged(address indexed _forWho, address indexed _byWhom, uint _oldAmount, uint _newAmount);
+
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed!");
+        _;
+    }
+
+    function isOwner() internal view returns(bool) {
+        return owner() == msg.sender;
+    }
+
+    function setAllowance(address _to, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_to, msg.sender, allowance[_to], _amount);
+        allowance[_to] = _amount;
+    }
+
+    function reduceAllowance(address _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(allowance[_to] - _amount >= 0, "Too much to reduce");
+        emit AllowanceChanged(_to, msg.sender, allowance[_to], allowance[_to] - _amount);
+        allowance[_to] -= _amount;
+    }
+}
---
+++ SharedWallet.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.6;
+
+import "./Allowance.sol";
+
+contract SharedWallet is Allowance{
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(address(this).balance >= _amount, "Not enough funds");
+        if(!isOwner()) {
+            reduceAllowance(_to, _amount);
+        }
+        emit MoneySent(_to, _amount);
+        _to.transfer(_amount);
+    }
+
+    function renounceOwnership() public override view onlyOwner {
+        revert("Can't renounce ownership here");
+    }
+
+    receive() external payable {
+        emit MoneyReceived(msg.sender, msg.value);
+    }
+}
